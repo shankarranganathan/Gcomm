f085068e124ff02525fd189a7bffe8c715a084b4
shankarranganathan <shankarmech100@gmail.com>
2024-09-30 09:32:40 +0530

    shancommit

---
+++ src/org/abc/Comm.java
@@ -0,0 +1,11 @@
+package org.abc;
+
+public class Comm {
+	public static void main(String[] args) {
+		System.out.println("shankar");
+		System.out.println("hansika");
+		System.out.println("jai");
+		System.out.println("siva");
+	}
+
+}
